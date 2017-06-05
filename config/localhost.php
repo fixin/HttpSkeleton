657118369ebaa057c93608aba9f73fe3afd233d7
Attila Jenei <contact@attilajenei.com>
2017-06-05 22:44:18 +0100

    Development mode

--- config/localhost.php
+++ config/localhost.php
@@ -1,4 +1,15 @@
 <?php
 return array_replace_recursive(require 'common.php', [
     // Custom config for 'localhost'
+    'resourceManager' => [
+        'definitions' => [
+            'config' => [
+                'options' => [
+                    'values' => [
+                        'development' => true
+                    ]
+                ]
+            ],
+        ]
+    ]
 ]);
