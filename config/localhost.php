93617569c74a9a87d02778a7b553683dc5ea8ff2
Attila Jenei <contact@attilajenei.com>
2017-08-02 00:46:18 +0100

    Error display

--- config/localhost.php
+++ config/localhost.php
@@ -1,6 +1,9 @@
 <?php
 return array_replace_recursive(require 'common.php', [
     // Custom config for 'localhost'
+    'application' => [
+        'showFatalError' => true,
+    ],
     'resourceManager' => [
         'definitions' => [
             'config' => [
