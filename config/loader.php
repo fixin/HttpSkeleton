2298e0adf964375107bb832e67067a88ba2c6414
Attila Jenei <contact@attilajenei.com>
2017-08-15 18:39:10 +0100

    Loader update

--- config/loader.php
+++ config/loader.php
@@ -1,13 +1,12 @@
 <?php
 
-// Check host is allowed
-$hosts = [
-    'localhost' => ['127.0.0.1'],
-    // Add host names and their IP addresses
-];
-$requestedHost = mb_strtolower($_SERVER['SERVER_NAME']);
+// Add your host names
+$hosts = ['yourHost'];
 
-if (!in_array($_SERVER['SERVER_ADDR'], $hosts[$requestedHost] ?? [])) {
+// Check host is allowed
+$requestedHost = gethostname();
+
+if (!in_array($requestedHost, $hosts)) {
     header("HTTP/1.1 404 Not Found");
     echo '<!DOCTYPE html><html><body><h1>Not Found</h1></body></html>';
 
@@ -17,5 +16,6 @@ if (!in_array($_SERVER['SERVER_ADDR'], $hosts[$requestedHost] ?? [])) {
 // Load config
 $topPath = dirname(__DIR__);
 $fixinPath = $topPath . '/vendor/fixin/fixin';
+$fixinPath = dirname(__DIR__, 2) . '/Fixin';
 
 return require "{$topPath}/config/{$requestedHost}.php";
